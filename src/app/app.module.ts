11f61dfe286f49ed2dc544d0923675026bda7c97
MiljanSjekloca01 <miljan.sjekloca.20@singimail.rs>
2024-04-21 22:18:19 +0200

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthComponent } from './auth/auth.component';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { RecipesModule } from './recipes/recipes.module';
 
 @NgModule({
   declarations: [
